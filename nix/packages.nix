312798263756989acf09c631223843e5f546d588
Zsolt Szende <zsolt@artificial.io>
2021-02-11 14:22:39 +0000

    Add optional pkg dependency compose

--- nix/packages.nix
+++ nix/packages.nix
@@ -3,16 +3,23 @@ let
   nixkellConfig = lib.importTOML ../nixkell.toml;
 
   haskellPackages = haskell.packages.${("ghc" + util.removeDot nixkellConfig.env.ghc)}.override {
-    overrides = _hfinal: hprev: {
-      replaceme =
-        let
-          filteredSrc = util.filterSrc ../. {
-            ignoreFiles = nixkellConfig.ignore.files;
-            ignorePaths = nixkellConfig.ignore.paths;
-          };
-        in
-        hprev.callCabal2nix "replaceme" filteredSrc { };
-    };
+    overrides =
+      let
+        depsFromDir = haskell.lib.packagesFromDirectory {
+          directory = ./packages;
+        };
+        ourPkg = _hfinal: hprev: {
+          replaceme =
+            let
+              filteredSrc = util.filterSrc ../. {
+                ignoreFiles = nixkellConfig.ignore.files;
+                ignorePaths = nixkellConfig.ignore.paths;
+              };
+            in
+            hprev.callCabal2nix "replaceme" filteredSrc { };
+        };
+      in
+      lib.composeExtensions depsFromDir ourPkg;
   };
 
   ghc = haskellPackages.ghc.withPackages (_ps:
