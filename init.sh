27f0ad2cc97a88ad5827a493d90750fdc5c24275
Zsolt Szende <zsolt@artificial.io>
2021-02-13 17:28:14 +0000

    Comment out the CI cachix config when init

--- init.sh
+++ init.sh
@@ -6,31 +6,32 @@ set -euo pipefail
 # Start in project root
 cd "$(dirname "${BASH_SOURCE[0]}")/"
 
-# Make sure the git repo is in fact nixkell before we nuke it
+# Make sure the git repo is in fact Nixkell's before nuking stuff
 repo_url=$(git config --get remote.origin.url)
 if [[ $(basename -s .git "$repo_url") != "nixkell" ]]; then
   echo "Not a nixkell repository, aborting..."
   exit 1
 fi
 
-# Nuke nixkell's .git and license
+# Nuke nixkell's .git
 rm -rf .git
-rm LICENSE
 
 project_name=${PWD##*/}
 
 # Blank slate readme
 echo "# $project_name" > README.md
 
-# Replace the dummy "replaceme" project names throughout with the actual one
-nix_files=$(find . -type f -name "*.nix")
-for i in $nix_files; do
+# Replace all dummy "replaceme" with the project name
+for i in $(find . -type f -name "*.nix"); do
   sed -i "s/replaceme/$project_name/g" "$i"
 done
 sed -i "s/replaceme/$project_name/g" ./package.yaml
 sed -i "s/replaceme/$project_name/g" ./bin/Main.hs
 
-# Fire up the nix shell
+# comment out the Nixkell specific cachix entry from the CI config
+sed -i -e "s/pwm/$project_name/" -e '/cachix-action/,+3 s/.*/# &/' '.github/workflows/nix.yml'
+
+# Create an .envrc and fire up the nix shell with it
 echo -e "use nix\nwatch_file nixkell.toml" > .envrc
 direnv allow .
 
