57eeb2ffc8eb0d4efd267b3986ce8614ee5289b5
Zsolt Szende <zsolt@artificial.io>
2022-07-16 17:34:05 +0200

    Reorg utils.nix
    No change just moving things around

--- nix/util.nix
+++ nix/util.nix
@@ -1,34 +1,5 @@
 { pkgs, lib, gitignoreFilter }:
 {
-  /* Remove dots from strings
-
-    > removeChar "." "9.2.3"
-    "923"
-  */
-  removeChar = c: s: lib.replaceStrings [ c ] [ "" ] s;
-
-  # Filter .gitignore as well as files and paths ignored in the nixkell config
-  filterSrc = path: { ignoreFiles ? [ ], ignorePaths ? [ ] }:
-    lib.cleanSourceWith {
-      src = path;
-      filter =
-        let
-          srcIgnored = gitignoreFilter path; # in let binding to memoize
-          relToPath = lib.removePrefix (toString path + "/");
-        in
-        path: type:
-          srcIgnored path type
-          && ! builtins.elem (baseNameOf path) ignoreFiles
-          && ! lib.any (d: lib.hasPrefix d (relToPath path)) ignorePaths;
-    };
-
-  /* Given a package return its derivation
-
-    > getDrv "haskellPackages.implicit-hie"
-    «derivation /nix/store/qb9j7iccna81dcs1szwryl4izli95w53-implicit-hie-0.1.2.7.drv»
-  */
-  getDrv = path: lib.getAttrFromPath (lib.splitString "." path) pkgs;
-
   /* Check whether a given list has an element
 
     > has "b" [ "a" "b" "c" ]
@@ -46,6 +17,35 @@
     [ "a" "b" "c" ]
   */
   remove = e: builtins.filter (x: e != x);
+
+  /* Remove dots from strings
+
+    > removeChar "." "9.2.3"
+    "923"
+  */
+  removeChar = c: s: lib.replaceStrings [ c ] [ "" ] s;
+
+  /* Given a package return its derivation
+
+    > getDrv "haskellPackages.implicit-hie"
+    «derivation /nix/store/qb9j7iccna81dcs1szwryl4izli95w53-implicit-hie-0.1.2.7.drv»
+  */
+  getDrv = path: lib.getAttrFromPath (lib.splitString "." path) pkgs;
+
+  # Filter .gitignore as well as files and paths ignored in the nixkell config
+  filterSrc = path: { ignoreFiles ? [ ], ignorePaths ? [ ] }:
+    lib.cleanSourceWith {
+      src = path;
+      filter =
+        let
+          srcIgnored = gitignoreFilter path; # in let binding to memoize
+          relToPath = lib.removePrefix (toString path + "/");
+        in
+        path: type:
+          srcIgnored path type
+          && ! builtins.elem (baseNameOf path) ignoreFiles
+          && ! lib.any (d: lib.hasPrefix d (relToPath path)) ignorePaths;
+    };
 
   # Speed up building by disabling a few steps
   leanPkg =
