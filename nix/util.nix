435e17f798c9acc65fa57fb1167dafcb893f046a
Zsolt Szende <zsolt@artificial.io>
2022-07-16 14:51:21 +0200

    Typo fix (#27)

--- nix/util.nix
+++ nix/util.nix
@@ -2,7 +2,7 @@
 {
   /* Remove dots from strings
 
-    nix-repl> removeDot "9.2.3"
+    > removeDot "9.2.3"
     "923"
   */
   removeDot = s: lib.replaceStrings [ "." ] [ "" ] s;
@@ -24,25 +24,25 @@
 
   /* Given a package return its derivation
 
-    nix-repl> getDrv "haskellPackages.implicit-hie"
+    > getDrv "haskellPackages.implicit-hie"
     «derivation /nix/store/qb9j7iccna81dcs1szwryl4izli95w53-implicit-hie-0.1.2.7.drv»
   */
   getDrv = path: lib.getAttrFromPath (lib.splitString "." path) pkgs;
 
-  /* Weather a given list has an element
+  /* Check whether a given list has an element
 
-    nix-repl> has "b" [ "a" "b" "c" ]
+    > has "b" [ "a" "b" "c" ]
     true
-    nix-repl> has "d" [ "a" "b" "c" ]
+    > has "d" [ "a" "b" "c" ]
     false
   */
   has = e: xs: lib.any (x: x == e) xs;
 
-  /* Remove element from a list
+  /* Remove an element from a list
 
-    nix-repl> remove "b" ["a" "b" "c"]
+    > remove "b" ["a" "b" "c"]
     [ "a" "c" ]
-    nix-repl> remove "d" ["a" "b" "c"]
+    > remove "d" ["a" "b" "c"]
     [ "a" "b" "c" ]
   */
   remove = e: builtins.filter (x: e != x);
